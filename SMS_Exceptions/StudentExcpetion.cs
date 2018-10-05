e38a44fed9a709f4e8f2f70d6caae1517a92b0df
Tushar530 <tusharsoni95@gmail.com>
2018-10-05 15:22:46 +0530

    Add files via upload

---
+++ SMS_Exceptions/StudentExcpetion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS_Exceptions
+{
+    public class StudentExcpetion : Exception
+    {
+        public StudentExcpetion( string errMsg ) : base (errMsg)
+        { 
+
+        }
+    }
+}
