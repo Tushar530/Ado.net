1e303c1ab29a4ab4c7d7fbca68cf27cf7494a416
Tushar530 <tusharsoni95@gmail.com>
2018-10-05 15:19:49 +0530

    Add files via upload

---
+++ SMS_Entities/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS_Entities
+{
+    public class Student
+    {
+
+        public int  Rollno { get; set; }
+
+        public string StudName { get; set; }
+
+        public   string  Gender  { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public double FeesPaid { get; set; }
+
+        public string MobileNO { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
